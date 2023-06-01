976f9a9208c81ad14b17f8ff702da9eb39dab2b9
Ladan Nazari <ladannazary@gmail.com>
2023-06-01 12:33:19 -0700

    front-end almost done

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,10 +1,19 @@
-
+import React, {Component} from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation/>
+      <Logo/>
+      <Rank />
+      <ImageLinkForm/>
+     {/* { 
+      <FaceRecognition/>}  */}
     </div>
   );
 }
