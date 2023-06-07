cc59d11e8d8f855ca5a985627386e434f19eaab6
Ladan Nazari <ladannazary@gmail.com>
2023-06-06 20:05:33 -0700

    input state created and working

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -7,16 +7,37 @@ import ParticlesBg from 'particles-bg'
 import "./App.css";
 
 
+// constructor(){
+//   super();
+//   this.state = {
+//   input = ‘’,
+//   }
+  
+//   onInputChange = (event) => {
+//   console.log(event)
+//   }
+//   }
+  
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+    setInput(event.target.value)
+  }
+
+
+  
+
   return (
     <div className="App">
       <ParticlesBg color="#ffffff" num={150}  type="cobweb" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* { 
-      <FaceRecognition/>}  */}
+      <ImageLinkForm onInputChange={onInputChange} />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
