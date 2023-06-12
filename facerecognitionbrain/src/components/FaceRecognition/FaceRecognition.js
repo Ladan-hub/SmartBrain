07c247e2cbc653e007feffd238bd56e1e60d1eaf
Ladan Nazari <ladannazary@gmail.com>
2023-06-12 13:11:07 -0700

    image size fixed

--- facerecognitionbrain/src/components/FaceRecognition/FaceRecognition.js
+++ facerecognitionbrain/src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const FaceRecognition = ({ imageUrl }) => {
   return (
-    <div className='center'>
-      {imageUrl && <img src={imageUrl} alt='' />}
+    <div className="center ma">
+      <div className="absolute mt2  ">
+        {imageUrl && <img src={imageUrl} alt="" width="500" height="auto" />}
+      </div>
     </div>
   );
 };
