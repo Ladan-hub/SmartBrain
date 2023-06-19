e4d88b7124a06210aff44e2cd67459c0e95c10c8
Ladan Nazari <ladannazary@gmail.com>
2023-06-19 10:35:09 -0700

    sign in button onClick added

--- frontend/src/components/Signin/Signin.js
+++ frontend/src/components/Signin/Signin.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Signin = ({onRouteChange}) => {
+
+
+
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
     <main className="pa4 black-80">
-  <form className="measure" onSubmit={() => onRouteChange('home')}>
+  <form className="measure">
     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
       <legend className="f1 fw6 ph0 mh0">Sign In</legend>
       <div className="mt3">
@@ -17,7 +20,7 @@ const Signin = ({onRouteChange}) => {
       </div>
     </fieldset>
     <div class="">
-      <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in"/>
+      <input class="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" onClick={() => onRouteChange('home')}/>
     </div>
     <div class="lh-copy mt3">
       <p onClick={() => onRouteChange('register')} className="f6 link dim black db">Register</p>
