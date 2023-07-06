c301c47096106e9750a91a35da5b5b7ec965ba47
Ladan Nazari <ladannazary@gmail.com>
2023-07-06 13:03:34 -0700

    entry count display is removed for now

--- frontend/src/components/Rank/Rank.js
+++ frontend/src/components/Rank/Rank.js
@@ -7,9 +7,9 @@ const Rank = ({ name, entries }) => {
   return (
     <div>
       <div className="white f3">
-        {`${name}, your current entry count is...`}
+        {`Welcome ${name}`}
       </div>
-      <div className="white f1">{`#${entries}`}</div>
+      {/* <div className="white f1">{`#${entries}`}</div> */}
     </div>
   );
 };
